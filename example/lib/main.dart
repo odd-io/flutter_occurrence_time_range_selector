0a02540b2948b9c3a0a1c8496189e8777e59179e
LukasPoque <me@lukas-poque.dev>
2024-07-19 15:23:47 +0200

    Update example to showcase creation of 50 events with 3 tags

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_occurrence_time_range_selector/flutter_occurrence_time_range_selector.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +10,98 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Occurrence Time Range Selector Demo'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  List<TimeEvent> generateRandomEvents(
+      DateTime start, DateTime end, List<String> tags, int count) {
+    Random random = Random();
+    List<TimeEvent> events = [];
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+    for (int i = 0; i < count; i++) {
+      DateTime randomDate = DateTime(
+        start.year,
+        start.month + random.nextInt(2),
+        start.day + random.nextInt(end.day - start.day),
+        random.nextInt(24),
+        random.nextInt(60),
+        random.nextInt(60),
+        random.nextInt(1000),
+      );
+      String randomTag = tags[random.nextInt(tags.length)];
+      events.add(TimeEvent(tag: randomTag, dateTime: randomDate));
+    }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    return events;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final events = generateRandomEvents(
+      DateTime(2024, 1, 1),
+      DateTime(2024, 2, 28),
+      ['Class A', 'Class B', 'Class C'],
+      500,
+    );
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          const Expanded(child: SizedBox()),
+          Expanded(
+            child: TimeRangeSelector(
+              startDate: DateTime(2024, 1, 12),
+              endDate: DateTime(2024, 2, 15),
+              events: events,
+              tagStyles: const {
+                'Class A': TagStyle(color: Colors.blue),
+                'Class B': TagStyle(color: Colors.red),
+                'Class C': TagStyle(color: Colors.green),
+              },
+              onRangeChanged: (DateTime newStart, DateTime newEnd) {
+                print('New range: $newStart to $newEnd');
+              },
+              onEventHover: (TimeEvent? event) {
+                if (event != null) {
+                  print('Hovered over: ${event.dateTime} - ${event.tag}');
+                }
+              },
+              style: const TimelineStyle(
+                axisColor: Colors.black,
+                axisLabelStyle: TextStyle(fontSize: 18, color: Colors.black),
+                barSpacing: 2,
+                backgroundColor: Colors.white,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Expanded(
+              child: SizedBox(
+            child: SelectableText(events.toString()),
+          )),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
